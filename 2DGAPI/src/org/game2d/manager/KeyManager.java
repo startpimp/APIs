7cd161af4b53cecbd6e095ff1f2806014d71326d
startpimp <41271417+startpimp@users.noreply.github.com>
2019-02-01 19:42:35 +0100

    Update KeyManager.java

--- 2DGAPI/src/org/game2d/manager/KeyManager.java
+++ 2DGAPI/src/org/game2d/manager/KeyManager.java
@@ -8,15 +8,13 @@ public class KeyManager implements KeyListener {
 	private boolean typing;
 
 	@Override
-	public void keyPressed(KeyEvent e) {typing = true;
-	System.out.println("Key pressed");}
+	public void keyPressed(KeyEvent e) {typing = true;}
 
 	@Override
 	public void keyReleased(KeyEvent e) {typing = false;}
 
 	@Override
-	public void keyTyped(KeyEvent e) {key = e.getKeyCode();
-	System.out.println("Typing");}
+	public void keyTyped(KeyEvent e) {key = e.getKeyCode();}
 
 	public int getKey() {return key;}
 	public boolean isTyping() {return typing;}
